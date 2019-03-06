b51c8bc8bacda69dd6c1f3224347dc301ef502fe
oliviel <olivielultimate@gmail.com>
2019-03-05 20:55:25 -0400

    Add BaseCard component

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="card mb-4">
+      <div class="card-header">
+        <i class="fas fa-user-circle"></i><strong class="ml-3 mt-2">Sofia Duran</strong>
+      </div>
+      <img src="https://i.pinimg.com/originals/99/b9/4a/99b94a97599ec1b1fe71558dc6cef21b.jpg" class="card-img-top img-fluid" alt="...">
+      <div class="card-body">
+        <p class="card-text"><strong>Sofia Duran</strong> Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div class="row">
+          <div class="col-6">
+            <button class="btn btn-light btn-lg btn-block"><i class="fas fa-heart"></i></button>
+          </div>
+          <div class="col-6">
+            <button class="btn btn-light btn-lg btn-block"><i class="fas fa-comments"></i></button>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
